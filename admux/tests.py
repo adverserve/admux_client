aea9acf87804655479ea9e0181e5c6b9ab1292df
Stephan Poetschner <stephan.poetschner@gmail.com>
2013-10-16 12:54:51 +0200

    + adding BaseTest to tests + renaming self.client -> self.fresh_api + adding self.api (already logged in)

--- admux/tests.py
+++ admux/tests.py
@@ -10,16 +10,32 @@ from django.test import TestCase
 
 from adserver.client import Client
 
-class ClientBasisTest(TestCase):
+class BaseTest(TestCase):
+    test_api_key = "C96A2442-1322-11E3-9E33-96237FA36B44"
 
     def setUp(self):
-        self.client = Client()
+        self.api = Client()
+
+        httpretty.register_uri(
+            httpretty.POST,
+            Client.get_url("/login"),
+            body='{ "api_key" : %s }' % self.test_api_key,
+            content_type="application/json"
+        )
+        self.api.login('strg', 'strg')
+
+        # initializing client without logging in
+        self.fresh_api = Client()
+
+
+
+class BasicTest(BaseTest):
 
     @httpretty.activate
     def test_login(self):
-        c = self.client
+        api = self.fresh_api
 
-        self.assertIsNone(c.api_key)
+        self.assertIsNone(api.api_key)
 
         httpretty.register_uri(
             httpretty.POST,
@@ -28,7 +44,7 @@ class ClientBasisTest(TestCase):
             content_type="application/json"
         )
 
-        key = c.login('strg', 'strg')
+        key = api.login('strg', 'strg')
 
         self.assertEqual("C96A2442-1322-11E3-9E33-96237FA36B44", key)
-        self.assertEqual("C96A2442-1322-11E3-9E33-96237FA36B44", c.api_key)
+        self.assertEqual("C96A2442-1322-11E3-9E33-96237FA36B44", api.api_key)
