2752091b537d1e1ad8639bcf368f96c2f9db9f0a
Stephan Poetschner <stephan.poetschner@gmail.com>
2013-10-16 11:07:44 +0200

    + adding adserver-module with client-code

---
+++ admux/client.py
@@ -0,0 +1,84 @@
+# coding: utf-8
+import logging
+log = logging.getLogger(__name__)
+
+import requests
+
+
+class ProtocolError(Exception):
+    pass
+
+class Client(object):
+    default_base_url = 'http://admux-demo.trust-box.at/v1/'
+    default_headers = {
+        'content-type': 'application/json',
+    }
+
+    api_key = None
+
+    @staticmethod
+    def get_url(url):
+        return '%s%s' % (Client.default_base_url, str(url))
+
+    def _request(self,
+                 method, url,
+                 requires_api_key=True,
+                 params=None, data=None, headers=None,
+                 *args, **kwargs):
+
+        if requires_api_key and not self.api_key:
+            raise ProtocolError("API Key not set for client session. ' \
+            'Try calling `login()` first.")
+
+        headers = dict(headers or {})
+        headers.update(self.default_headers)
+
+        absolute_url = Client.get_url(url)
+
+        log.info("Adserver-Request: %s, %s", absolute_url, params)
+
+        resp = requests.request(method, absolute_url,
+                                params=params,
+                                data=data,
+                                headers=headers,
+                                *args, **kwargs)
+
+        log.info("Adserver-Response: %s, (%s)",
+                 resp.status_code,
+                 resp.text)
+
+        resp.raise_for_status()
+
+        return resp.json()
+
+    def login(self, username, password):
+        """
+        returns the api_key for given login-credentials
+        (also sets the api_key as an internal variable)
+        """
+
+        data = self._request('POST', '/login', params={
+            'username' : username,
+            'password' : password,
+        }, requires_api_key=False)
+
+        self.api_key = data['api_key']
+
+        return self.api_key
+
+
+
+if __name__ == '__main__':
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+
+    formatter = logging.Formatter(
+        '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    )
+    ch.setFormatter(formatter)
+    log.addHandler(ch)
+
+    log.setLevel(logging.DEBUG)
+
+    c = Client()
+    c.login(username='styria', password='styria')
---
+++ admux/tests.py
@@ -0,0 +1,32 @@
+"""
+Run "manage.py test"
+"""
+
+import httpretty
+
+from django.test import TestCase
+
+from adserver.client import Client
+
+class ClientBasisTest(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+
+    @httpretty.activate
+    def test_login(self):
+        c = self.client
+
+        self.assertIsNone(c.api_key)
+
+        httpretty.register_uri(
+            httpretty.POST,
+            Client.get_url("/login"),
+            body='{ "api_key" : "C96A2442-1322-11E3-9E33-96237FA36B44" }',
+            content_type="application/json"
+        )
+
+        key = c.login('strg', 'strg')
+
+        self.assertEqual("C96A2442-1322-11E3-9E33-96237FA36B44", key)
+        self.assertEqual("C96A2442-1322-11E3-9E33-96237FA36B44", c.api_key)
