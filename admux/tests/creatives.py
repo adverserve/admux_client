2736a7ce51f49be5b916ac1f3bdff149dd17c407
Stephan Poetschner <stephan.poetschner@gmail.com>
2013-10-18 11:46:08 +0200

    + splitting creatives tests

--- admux/tests/creatives.py
+++ admux/tests/creatives.py
@@ -101,6 +101,80 @@ class CreativesMixin(object):
 
         return data
 
+
+class AddCreativesTest(CreativesMixin, OrdersMixin, CampaignsMixin,
+                       PlacementsMixin, WebsitesMixin,
+                       LoginMixin,
+                       BaseMixin, TestCase):
+
+    def setUp(self):
+        self.api = Client()
+        self._login(*self.credentials)
+
+        self._get_websites()
+        self._get_placements()
+
+        self._order_create(name=self.order_name)
+        self._campaign_create(uuid=self.order_id, name=self.campaign_name)
+
+
+    def tearDown(self):
+        self._order_delete(uuid=self.order_id)
+        self._campaign_delete(uuid=self.campaign_id)
+        self._creative_delete(uuid=self.creative_id)
+
+    @fake_requests
+    def test_create(self):
+        ''' Creating creative '''
+        data = self._creative_create(uuid=self.campaign_id,
+                                     html=self.creative_html,
+                                     placement=self.placement_id)
+        self.assertTrue(u'creative' in data)
+        self.assertEquals(self.creative_id, data[u'creative'])
+
+        self.assertTrue(u'job' in data)
+
+        if httpretty.httpretty.is_enabled():
+            request_body = httpretty.last_request().body
+            request_body = json.loads(request_body)
+
+            self.assertTrue(u'html' in request_body)
+            self.assertTrue(u'placement' in request_body)
+
+
+class CreativesTest(CreativesMixin, OrdersMixin, CampaignsMixin,
+                    PlacementsMixin, WebsitesMixin,
+                    LoginMixin,
+                    BaseMixin, TestCase):
+
+    def setUp(self):
+        self.api = Client()
+        self._login(*self.credentials)
+
+        self._get_websites()
+        self._get_placements()
+
+        self._order_create(name=self.order_name)
+        self._campaign_create(uuid=self.order_id, name=self.campaign_name)
+
+        self._creatives_create(uuid=self.campaign_id,
+                               html=self.creative_html,
+                               placement=self.placement_id)
+
+    def tearDown(self):
+        self._order_delete(uuid=self.order_id)
+        self._campaign_delete(uuid=self.campaign_id)
+
+    @fake_requests
+    def test_delete(self):
+        ''' Deleting creative '''
+        data = self._creative_delete(uuid=self.creative_id)
+        self.assertTrue(u'message' in data)
+        self.assertEquals(u'Deleted', data[u'message'])
+
+        self.assertTrue(u'job' in data)
+
+
 class CreativesTest(CreativesMixin, OrdersMixin, CampaignsMixin,
                     PlacementsMixin, WebsitesMixin,
                     LoginMixin,
@@ -175,34 +249,6 @@ class CreativesTest(CreativesMixin, OrdersMixin, CampaignsMixin,
         self.assertTrue(u'uuid' in data)
 
 
-    @fake_requests
-    def test_delete(self):
-        data = self._creative_delete(uuid=self.creative_id)
-        self.assertTrue(u'message' in data)
-        self.assertEquals(u'Deleted', data[u'message'])
-
-        self.assertTrue(u'job' in data)
-        self.assertEquals(self.job_id, data[u'job'])
-
-
-    @fake_requests
-    def test_create(self):
-        data = self._creative_create(uuid=self.campaign_id,
-                                     html=self.creative_html,
-                                     placement=self.placement_id)
-        self.assertTrue(u'creative' in data)
-        self.assertEquals(self.creative_id, data[u'creative'])
-
-        self.assertTrue(u'job' in data)
-        self.assertEquals(self.job_id, data[u'job'])
-
-        if httpretty.httpretty.is_enabled():
-            request_body = httpretty.last_request().body
-            request_body = json.loads(request_body)
-
-            self.assertTrue(u'html' in request_body)
-            self.assertTrue(u'placement' in request_body)
-
 
     @fake_requests
     def test_update(self):
@@ -229,6 +275,5 @@ class CreativesTest(CreativesMixin, OrdersMixin, CampaignsMixin,
         self.assertEquals(u'Updated', data[u'message'])
 
         self.assertTrue(u'job' in data)
-        self.assertEquals(self.job_id, data[u'job'])
 
 
