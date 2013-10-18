03c1adb89817cf86b4e0e63f97187ac0f43a1f5c
Stephan Poetschner <stephan.poetschner@gmail.com>
2013-10-18 11:51:44 +0200

    + splitting tests for images

--- admux/tests/images.py
+++ admux/tests/images.py
@@ -106,6 +106,92 @@ class ImagesMixin(object):
         return data
 
 
+class AddImagesTest(ImagesMixin,
+                    CreativesMixin, OrdersMixin, CampaignsMixin,
+                    PlacementsMixin, WebsitesMixin,
+                    LoginMixin,
+                    BaseMixin, TestCase):
+
+    def setUp(self):
+        self.api = Client()
+        self._login(*self.credentials)
+
+        self._get_websites()
+        self._get_placements()
+
+        self._order_create(name=self.order_name)
+        self._campaign_create(uuid=self.order_id, name=self.campaign_name)
+
+        self._creative_create(uuid=self.campaign_id,
+                              html=self.creative_html,
+                              placement=self.placement_id)
+
+
+    def tearDown(self):
+        self._order_delete(uuid=self.order_id)
+        self._campaign_delete(uuid=self.campaign_id)
+        self._creative_delete(uuid=self.creative_id)
+        self._image_delete(uuid=self.image_id)
+
+
+    @fake_requests
+    def test_create(self):
+        ''' Creating image '''
+
+        data = self._image_create(uuid=self.creative_id,
+                                  file_name='sample.jpg',
+                                  file_object=StringIO("sample data"))
+        self.assertTrue(u'image' in data)
+        self.assertEquals(self.image_id, data[u'image'])
+
+        self.assertTrue(u'job' in data)
+
+        if httpretty.httpretty.is_enabled():
+            request_body = httpretty.last_request().body
+            request_body = json.loads(request_body)
+
+            self.assertTrue(u'filename' in request_body)
+            self.assertTrue(u'data' in request_body)
+
+
+class RemoveImagesTest(ImagesMixin,
+                       CreativesMixin, OrdersMixin, CampaignsMixin,
+                       PlacementsMixin, WebsitesMixin,
+                       LoginMixin,
+                       BaseMixin, TestCase):
+
+    def setUp(self):
+        self.api = Client()
+        self._login(*self.credentials)
+
+        self._get_websites()
+        self._get_placements()
+
+        self._order_create(name=self.order_name)
+        self._campaign_create(uuid=self.order_id, name=self.campaign_name)
+
+        self._image_create(uuid=self.creative_id,
+                           file_name='sample.jpg',
+                           file_object=StringIO("sample data"))
+
+
+    def tearDown(self):
+        self._order_delete(uuid=self.order_id)
+        self._campaign_delete(uuid=self.campaign_id)
+        self._creative_delete(uuid=self.creative_id)
+        self._creative_delete(uuid=self.creative_id)
+
+    @fake_requests
+    def test_delete(self):
+        ''' Deleting image '''
+        data = self._image_delete(uuid=self.image_id)
+        self.assertTrue(u'message' in data)
+        self.assertEquals(u'Deleted', data[u'message'])
+
+        self.assertTrue(u'job' in data)
+
+
+
 class ImagesTest(ImagesMixin,
                  CreativesMixin, OrdersMixin, CampaignsMixin,
                  PlacementsMixin, WebsitesMixin,
@@ -137,6 +223,8 @@ class ImagesTest(ImagesMixin,
 
     @fake_requests
     def test_list(self):
+        ''' Listing images '''
+
         data = self._images_list(uuid=self.creative_id)
         self.assertTrue(u'images' in data)
 
@@ -152,6 +240,8 @@ class ImagesTest(ImagesMixin,
 
     @fake_requests
     def test_detail(self):
+        ''' List details of single image '''
+
         body = r'' \
             r'''
             {
@@ -192,35 +282,6 @@ class ImagesTest(ImagesMixin,
 
 
     @fake_requests
-    def test_delete(self):
-        data = self._image_delete(uuid=self.image_id)
-        self.assertTrue(u'message' in data)
-        self.assertEquals(u'Deleted', data[u'message'])
-
-        self.assertTrue(u'job' in data)
-        self.assertEquals(self.job_id, data[u'job'])
-
-
-    @fake_requests
-    def test_create(self):
-        data = self._image_create(uuid=self.creative_id,
-                                  file_name='sample.jpg',
-                                  file_object=StringIO("sample data"))
-        self.assertTrue(u'image' in data)
-        self.assertEquals(self.image_id, data[u'image'])
-
-        self.assertTrue(u'job' in data)
-        self.assertEquals(self.job_id, data[u'job'])
-
-        if httpretty.httpretty.is_enabled():
-            request_body = httpretty.last_request().body
-            request_body = json.loads(request_body)
-
-            self.assertTrue(u'filename' in request_body)
-            self.assertTrue(u'data' in request_body)
-
-
-    @fake_requests
     def test_update(self):
         body = r'' \
             r'''
@@ -245,6 +306,5 @@ class ImagesTest(ImagesMixin,
         self.assertEquals(u'Updated', data[u'message'])
 
         self.assertTrue(u'job' in data)
-        self.assertEquals(self.job_id, data[u'job'])
 
 
